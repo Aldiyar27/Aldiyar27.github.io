2b29b3c7b6574a71ba711956102e69edf8fed0d9
Aldiyar <aldi.year@gmail.com>
2024-03-03 14:38:43 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const searchInput = document.getElementById("searchInput");
+    const searchButton = document.getElementById("searchButton");
+    const errorContainer = document.getElementById("errorContainer");
+    const girlsContainer = document.getElementById("girlsContainer");
+  
+    searchButton.addEventListener("click", handleSearch);
+  
+    async function handleSearch() {
+      const searchQuery = searchInput.value;
+      errorContainer.textContent = ''; // Clear previous error message
+      girlsContainer.innerHTML = ''; // Clear previous search results
+  
+      try {
+        const response = await fetch(`https://65ce2e5fc715428e8b402554.mockapi.io/girls?search=${searchQuery}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        displayGirls(data);
+      } catch (error) {
+        errorContainer.textContent = error.message;
+      }
+    }
+  
+    function displayGirls(girls) {
+      girls.forEach(girl => {
+        const girlDiv = document.createElement('div');
+        const girlImage = document.createElement('img');
+        girlImage.src = girl.photo;
+        girlImage.alt = girl.name;
+        girlImage.style.width = '100px';
+        girlImage.style.height = '100px';
+        girlImage.style.objectFit = 'cover';
+        girlDiv.appendChild(girlImage);
+        girlsContainer.appendChild(girlDiv);
+      });
+    }
+});
+
+
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+};
